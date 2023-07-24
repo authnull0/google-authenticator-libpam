95b9ccd981877945c0e04b74cbaa871fbbe06d03
Ubuntu <asifali@test-server1.np402gaicniuhbtwuqf31vev5e.bx.internal.cloudapp.net>
2023-07-24 07:45:11 +0000

    fix syntax

--- src/pam_google_authenticator.c
+++ src/pam_google_authenticator.c
@@ -303,7 +303,7 @@ int google_authenticator(pam_handle_t *pamh,
 
   struct passwd *pwd = getpwnam(user);
   if (pwd != NULL) {
-    userExistLocallyFlag = true
+    userExistLocallyFlag = true;
     printf("User %s does not exist.\n", user);
   }
   log_message(LOG_INFO,pamh,"retvalusere %s",user);
@@ -348,7 +348,7 @@ int google_authenticator(pam_handle_t *pamh,
   char command[100];
   int len;
 
-  if(userExistFlag) {
+  if(userExistLocallyFlag) {
     len = snprintf(command, sizeof(command), "/bin/bash ${cwd}/did.sh %s",user);
     output =popen(command, "r");// update this location based on user path , and copy the script inside src/ to user path (if reqd)
   
