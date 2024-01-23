56e74b3aa39554baf96e89c51f3ad44814add617
Hussain <pshussain@gmail.com>
2024-01-23 20:43:32 +0530

    AV-689 Added print for requestId

--- src/pam_google_authenticator.c
+++ src/pam_google_authenticator.c
@@ -370,7 +370,7 @@ int google_authenticator(pam_handle_t *pamh,
           printf("Copy paste the URL and login: %s\n", line);
           // Check if there is a second token
           // Get the second token
-          extractSecondItem(&line, response, 32, delimiter);
+          extractSecondItem(line, response, 32, delimiter);
           requestId = line;
           break;
         }
