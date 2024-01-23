06f877c1003fb2e12f37fa3cafcf3c8e3a26a87e
Hussain <pshussain@gmail.com>
2024-01-23 21:26:31 +0530

    AV-689 Added print for requestId

--- src/pam_google_authenticator.c
+++ src/pam_google_authenticator.c
@@ -439,7 +439,7 @@ void extractSecondItem(char *inputString, char *result, size_t resultSize, char 
     i++;
     // Copy characters to the result until the second delimiter is found or the end of the string
     while (inputString[i] != '\0' && inputString[i] != delimiter && count < resultSize ) {
-        printf("Char Result %c\n", inputString[i]);
+        printf("Char Result %c , %d, %d\n", inputString[i], i, count);
         result[count] = inputString[i];
         count++;
         i++;
