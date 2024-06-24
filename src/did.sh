f03bd659a466fc7851aa9f1a1ed76d05a91cd04d
Sharan AC <127299857+SharanAC@users.noreply.github.com>
2024-06-24 11:53:16 +0530

    Update did.sh

--- src/did.sh
+++ src/did.sh
@@ -1,74 +1,67 @@
 #!/bin/bash
-
+ 
 echo "hello , Starting the Assertion"
-string=`groups $USER`
+string=$(groups $USER)
 prefix="$USER : "
 groupsStr=${string#"$prefix"}
 echo "hiiii" ${PAM_USER}
-#echo "Env Vars:"
-#env
-
-hoststr=`hostname -f`
+ 
+hoststr=$(hostname -f)
 prefixhost="Static hostname: "
 hostname=${hoststr#"$prefixhost"}
-#echo $hostname
-
-input=`tail -100 /var/log/auth.log | grep -oP '(?<=Postponed publickey for )\w+' | tail -1`
-#echo "imput ${input}"
-value=`id -Gn $1`
-
-
-#user=`$1`
+ 
+input=$(tail -100 /var/log/auth.log | grep -oP '(?<=Postponed publickey for )\w+' | tail -1)
+ 
+value=$(id -Gn $1)
+ 
 user=$1
-
-#file="./conf.properties"
-
-#while IFS='=' read -r key value
-#do
-#    key=$(echo $key | tr '.' '_')
-#    eval ${key}=\${value}
-#done < "$file"
-
-#echo "User Id (ssh.pam.user) =         " ${ssh_pam_user}
-#echo "user password (ssh.pam.groups) = " ${ssh_pam_groups}
+ 
+# Get the user ID of the specified username
+userId=$(id -u $user)
+echo "User ID: $userId"
+ 
+# Determine credential type based on user ID range
+if [[ $userId -ge 1 && $userId -le 999 ]]; then
+  credentialType="SERVICEACCOUNT"
+else
+  credentialType="EPM"
+fi
+echo "Credential Type: $credentialType"
+ 
 uuid=$(uuidgen)
 echo $uuid
-
+ 
 generate_post_data()
 {
   cat <<EOF
 {
-  "username": "`echo ${user}`" ,
-  "credentialType": "EPM",
-  "hostname": "`echo $hoststr`",
-  "groupName": "`echo ${value}`",
+  "username": "$(echo ${user})",
+  "credentialType": "$credentialType",
+  "hostname": "$(echo ${hoststr})",
+  "groupName": "$(echo ${value})",
   "orgId": 84,
   "tenantId": 7,
-  "requestId": "`echo $uuid`"
+  "requestId": "$(echo $uuid)",
+  "userId": "$(echo $userId)"
 }
 EOF
 }
-
+ 
 echo $(generate_post_data)
-
-
+ 
 echo "Script executed from: ${PWD}"
-echo "First agr is $1"
-
-
-
-RES=$(curl -H "Accept: application/json" -H "Content-Type:application/json" --connect-timeout 50 -m 50 -X POST --data "$(generate_post_data)"  "https://v1.api.authnull.com/authnull0/api/v1/authn/v3/do-authenticationV4")
+echo "First arg is $1"
+ 
+RES=$(curl -H "Accept: application/json" -H "Content-Type:application/json" --connect-timeout 50 -m 50 -X POST --data "$(generate_post_data)"  "https://prod.api.authnull.com/authnull0/api/v1/authn/v3/do-authenticationV4")
 SSO=$(echo "$RES" | jq -r '.ssoUrl')
 requestId=$(echo "$RES" | jq '.requestId')
-
+ 
 if [[ $requestId != "null" ]]; then
-        echo "$SSO"
+  echo "SSO URL: $SSO"
 else
-        echo "*"
+  echo "*"
 fi
-
-#echo "Response Body: $RESPONSE_BODY"
+ 
 content=$(sed '$ d' <<< "$requestId")
-
-#echo "$content"
+ 
 return 0
