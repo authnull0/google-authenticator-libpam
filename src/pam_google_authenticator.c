a000bd7baa58a8600bc0f1160529d3fc0d303f07
muthu-durai <muthudurai1011@gmail.com>
2024-06-25 11:44:16 +0530

    added source ip

--- src/pam_google_authenticator.c
+++ src/pam_google_authenticator.c
@@ -313,6 +313,14 @@ int google_authenticator(pam_handle_t *pamh,
   }
   log_message(LOG_INFO,pamh,"retvalusere %s",user);
 
+  //identify sourceip
+  retval = pam_get_item(pamh, PAM_RHOST, (const void **)&host);
+  if (retval != PAM_SUCCESS) {
+    log_message(LOG_INFO,pamh,"retval",retval);
+  }
+  //store the source ip in a variable
+  log_message(LOG_INFO,pamh,"sourceip",host);
+
 
   char cwd[PATH_MAX];
   if (getcwd(cwd, sizeof(cwd)) != NULL) {
